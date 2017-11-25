0ec19fbea5072b77a9e36dfba222427b533fb1d0
Ani <a.megerdoumian@tue.nl>
2017-11-25 19:42:48 +0100

    added buttons and some changes

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,43 +1,61 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.URI;
 import java.util.List;
+
 
 public class GameOfLife extends JFrame implements ActionListener {
 
     private Grid grid;
+
     private JMenuBar mb_menu;
-    private JMenu m_file, m_help;
-    private JMenuItem mi_file_exit;
+    private JMenu m_help;
     private JMenuItem mi_help_about, mi_help_source;
-//    private static JFrame gameFrame = new JFrame("Game of Life");
-//    static JTextArea tArea;
+    JButton startGameBtn = new JButton("Start");
+    JButton stopGameBtn = new JButton("Stop");
+    JButton resetGameBtn = new JButton("Reset");
+    JButton randomFillBtn = new JButton("Random Pattern");
+    JButton newGame = new JButton("New Game");
     private Thread game;
 
+
     public GameOfLife() {
+
         // Setup menu
         mb_menu = new JMenuBar();
         setJMenuBar(mb_menu);
-        m_file = new JMenu("File");
-        mb_menu.add(m_file);
         m_help = new JMenu("Help");
         mb_menu.add(m_help);
-        mi_file_exit = new JMenuItem("Exit");
-        mi_file_exit.addActionListener(this);
-        m_file.add(new JSeparator());
-        m_file.add(mi_file_exit);
-
         mi_help_about = new JMenuItem("About");
         mi_help_about.addActionListener(this);
         mi_help_source = new JMenuItem("Source");
         mi_help_source.addActionListener(this);
         m_help.add(mi_help_about);
         m_help.add(mi_help_source);
+
+        setLayout(new BorderLayout(3, 3));  // BorderLayout with 3-pixel gaps.
         // Setup game board
         this.grid = new Grid();
-        add(grid);
+        add(grid, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();  // The subpanel that holds the buttons.
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        startGameBtn.addActionListener(this);   // The CardPanel listens for events.
+        buttonPanel.add(startGameBtn);
+
+        stopGameBtn.addActionListener(this);
+        buttonPanel.add(stopGameBtn);
+
+        resetGameBtn.addActionListener(this);
+        buttonPanel.add(resetGameBtn);
+
+        randomFillBtn.addActionListener(this);
+        buttonPanel.add(randomFillBtn);
+
+        newGame.addActionListener(this);
+        buttonPanel.add(newGame);
     }
 
     public void tick() {
@@ -45,13 +63,6 @@ public class GameOfLife extends JFrame implements ActionListener {
     }
 
     public void randomInit(List<Cell> initialPattern, int dimension) {
-//        tArea = new JTextArea(dimension, dimension);
-//        tArea.setFont(new Font(Font.MONOSPACED, Font.BOLD, 30));
-//        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        gameFrame.getContentPane().add(tArea, BorderLayout.CENTER);
-//        gameFrame.setSize(500, 300);
-//        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        gameFrame.setVisible(true);
         this.grid.setInitialPattern(initialPattern);
     }
 
@@ -60,9 +71,9 @@ public class GameOfLife extends JFrame implements ActionListener {
     }
 
     public void print() {
-       // tArea.setText(grid.print());
         try {
             Thread.sleep(1000);
+            grid.run();
         } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
@@ -72,35 +83,16 @@ public class GameOfLife extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource().equals(mi_file_exit)) {
-            // Exit the game
-            System.exit(0);
-        }  /*else if (e.getSource().equals(mi_game_autofill)) {//TODO
-            final JFrame f_autoFill = new JFrame();
-            f_autoFill.setTitle("Autofill");
-            f_autoFill.setSize(360, 60);
-            f_autoFill.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - f_autoFill.getWidth()) / 2,
-                    (Toolkit.getDefaultToolkit().getScreenSize().height - f_autoFill.getHeight()) / 2);
-            f_autoFill.setResizable(false);
-            JPanel p_autoFill = new JPanel();
-            p_autoFill.setOpaque(false);
-            f_autoFill.add(p_autoFill);
-            p_autoFill.add(new JLabel("What percentage should be filled? "));
-            Object[] percentageOptions = {"Select", 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 95};
-            final JComboBox cb_percent = new JComboBox(percentageOptions);
-            p_autoFill.add(cb_percent);
-            cb_percent.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (cb_percent.getSelectedIndex() > 0) {
-                        grid.resetBoard();
-                        grid.randomlyFillBoard((Integer) cb_percent.getSelectedItem());
-                        f_autoFill.dispose();
-                    }
-                }
-            });
-            f_autoFill.setVisible(true);
-        }*/ else if (e.getSource().equals(mi_help_about)) {
+        if (e.getSource().equals(startGameBtn)) {
+            grid.setGameBeingPlayed(true);
+        } else if (e.getSource().equals(resetGameBtn)) {
+            grid.resetBoard();
+        } else if (e.getSource().equals(stopGameBtn)) {
+            grid.setGameBeingPlayed(false);
+        } else if (e.getSource().equals(randomFillBtn)) {
+            grid.resetBoard();
+            grid.randomlyFillBoard();
+        } else if (e.getSource().equals(mi_help_about)) {
             JOptionPane.showMessageDialog(null, "Conway's game of life was a cellular animation devised by the mathematician John Conway.\nThis Java, swing based implementation was created by OOTI-2017.\n\n");
         }
     }
--- src/Grid.java
+++ src/Grid.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -8,76 +10,62 @@ import java.util.List;
 public class Grid extends JPanel implements ComponentListener, MouseListener, MouseMotionListener, Runnable {
 
     private static final int MAX_SIZE = 100;
-    private int dimension = 10;
-    private List<Cell>[][] cells = new ArrayList[dimension][dimension];
+
+    private List<Cell>[][] cells;
     private static final int BLOCK_SIZE = 10;//for GUI
     private Dimension d_gameBoardSize = null; //for GUI
     private ArrayList<Point> point = new ArrayList<Point>(0);//for GUI
+    private Thread game;
 
     public Grid() {
         // Add resizing listener
         addComponentListener(this);
         addMouseListener(this);
         addMouseMotionListener(this);
-        for (int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
-                Cell cell = new Cell(i, j);
-                cells[i][j] = new ArrayList<Cell>();
-                cells[i][j].add(cell);
-            }
-        }
     }
 
     public void setInitialPattern(List<Cell> initialPattern) {
-
         for (int i = 0; i < initialPattern.size(); i++) {
             Cell cell = initialPattern.get(i);
-            cells[cell.getPositionX()][cell.getPositionY()] = new ArrayList<Cell>();
-            cells[cell.getPositionX()][cell.getPositionY()].add(cell);
+            cells[cell.getPositionX()][cell.getPositionY()].get(0).setCurrentStatus(Status.ALIVE);
         }
-
         addTheNeighbors();
     }
 
     private void addTheNeighbors() {
-
-        int dimension = this.dimension;
-        for (int i = 0; i < dimension; i++) {
-
-            for (int j = 0; j < dimension; j++) {
-
-                if (i < dimension && j < dimension && i >= 1 && j >= 1) {
+        int dimensionX = d_gameBoardSize.width - 1;
+        int dimensionY = d_gameBoardSize.height - 1;
+        for (int i = 0; i < dimensionX; i++) {
+            for (int j = 0; j < dimensionY; j++) {
+                if (i >= 1 && j >= 1) {
                     cells[i][j].get(0).addNeighbor(cells[i - 1][j - 1].get(0));
                 }
-                if (i < dimension && j < dimension - 1 && i >= 1 && j >= 0) {
+                if (j < dimensionY - 1 && i >= 1) {
                     cells[i][j].get(0).addNeighbor(cells[i - 1][j + 1].get(0));
                 }
-                if (i < dimension - 1 && j < dimension && i >= 0 && j >= 1) {
+                if (i < dimensionX - 1 && j >= 1) {
                     cells[i][j].get(0).addNeighbor(cells[i + 1][j - 1].get(0));
                 }
-                if (i < dimension - 1 && j < dimension - 1 && i >= 0 && j >= 0) {
+                if (i < dimensionX - 1 && j < dimensionY - 1) {
                     cells[i][j].get(0).addNeighbor(cells[i + 1][j + 1].get(0));
                 }
-                if (i < dimension && j < dimension && i >= 1 && j >= 0) {
+                if (i >= 1) {
                     cells[i][j].get(0).addNeighbor(cells[i - 1][j].get(0));
                 }
-                if (i < dimension && j < dimension && i >= 0 && j >= 1) {
+                if (j >= 1) {
                     cells[i][j].get(0).addNeighbor(cells[i][j - 1].get(0));
                 }
-                if (i < dimension && j < dimension - 1 && i >= 0 && j >= 0) {
+                if (j < dimensionY - 1) {
                     cells[i][j].get(0).addNeighbor(cells[i][j + 1].get(0));
                 }
-                if (i < dimension - 1 && j < dimension && i >= 0 && j >= 0) {
+                if (i < dimensionX - 1) {
                     cells[i][j].get(0).addNeighbor(cells[i + 1][j].get(0));
                 }
             }
-
         }
-
     }
 
     public void evolve() {
-
         for (List<Cell>[] cell : cells) {
             for (List<Cell> aCell : cell) {
                 aCell.get(0).evaluate();
@@ -97,7 +85,7 @@ public class Grid extends JPanel implements ComponentListener, MouseListener, Mo
 
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[i].length; j++) {
-                result += cells[i][j].get(0).getCurrentStatus().toString() + " ";
+                result += cells[i][j].get(0).getCurrentStatus().name().toString() + " ";
             }
 
             result += "\n";
@@ -106,13 +94,6 @@ public class Grid extends JPanel implements ComponentListener, MouseListener, Mo
         return result;
     }
 
-    public int getDimension() {
-        return dimension;
-    }
-
-    public void setDimension(int dimension) {
-        this.dimension = dimension;
-    }
 
     @Override
     public void componentResized(ComponentEvent e) {
@@ -120,16 +101,18 @@ public class Grid extends JPanel implements ComponentListener, MouseListener, Mo
         d_gameBoardSize = new Dimension(getWidth() / BLOCK_SIZE - 2, getHeight() / BLOCK_SIZE - 2);
         updateArraySize();
     }
+
     private void updateArraySize() {
         ArrayList<Point> removeList = new ArrayList<Point>(0);
         for (Point current : point) {
-            if ((current.x > d_gameBoardSize.width-1) || (current.y > d_gameBoardSize.height-1)) {
+            if ((current.x > d_gameBoardSize.width - 1) || (current.y > d_gameBoardSize.height - 1)) {
                 removeList.add(current);
             }
         }
         point.removeAll(removeList);
         repaint();
     }
+
     @Override
     public void componentMoved(ComponentEvent e) {
 
@@ -185,6 +168,56 @@ public class Grid extends JPanel implements ComponentListener, MouseListener, Mo
     @Override
     public void run() {
 
+        boolean[][] gameBoard = new boolean[d_gameBoardSize.width + 2][d_gameBoardSize.height + 2];
+        GameOfLife gol = new GameOfLife();
+        List<Cell> initialPattern = new ArrayList<Cell>();
+
+        cells = new ArrayList[d_gameBoardSize.width][d_gameBoardSize.height];
+        for (int i = 0; i < d_gameBoardSize.width; i++) {
+            for (int j = 0; j < d_gameBoardSize.height; j++) {
+                Cell cell = new Cell(i, j);
+                cells[i][j] = new ArrayList<Cell>();
+                cells[i][j].add(cell);
+            }
+        }
+        for (Point current : point) {
+            Cell initialPatternCell = new Cell(current.x, current.y, Status.ALIVE);
+            initialPattern.add(initialPatternCell);
+        }
+        setInitialPattern(initialPattern);
+        evolve();
+        resetBoard();
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                if (cells[i][j].get(0).getCurrentStatus().equals(Status.ALIVE)) {
+                    Point p = new Point();
+                    p.x = i;
+                    p.y = j;
+                    point.add(p);
+                }
+            }
+        }
+        repaint();
+        try {
+            Thread.sleep(800);
+            run();
+        } catch (InterruptedException ex) {
+        }
+    }
+
+    public void setGameBeingPlayed(boolean isBeingPlayed) {
+        if (isBeingPlayed) {
+            game = new Thread(this);
+            game.start();
+        }
+        if(!isBeingPlayed){
+            game.interrupt();
+            try{
+            game.join();
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+        }
     }
 
     public void addPoint(MouseEvent me) {
@@ -196,22 +229,27 @@ public class Grid extends JPanel implements ComponentListener, MouseListener, Mo
     }
 
     public void addPoint(int x, int y) {
-        if (!point.contains(new Point(x,y))) {
-            point.add(new Point(x,y));
+
+        if (!point.contains(new Point(x, y))) {
+            point.add(new Point(x, y));
         }
         repaint();
     }
+
     public void removePoint(int x, int y) {
-        point.remove(new Point(x,y));
+        point.remove(new Point(x, y));
     }
+
     public void resetBoard() {
         point.clear();
     }
-    public void randomlyFillBoard(int percent) {
-        for (int i=0; i<d_gameBoardSize.width; i++) {
-            for (int j=0; j<d_gameBoardSize.height; j++) {
-                if (Math.random()*100 < percent) {
-                    addPoint(i,j);
+
+    public void randomlyFillBoard() {
+        int percent =(int) Math.floor(Math.random() * 100) + 1;
+        for (int i = 0; i < d_gameBoardSize.width; i++) {
+            for (int j = 0; j < d_gameBoardSize.height; j++) {
+                if (Math.random() * 100 < percent) {
+                    addPoint(i, j);
                 }
             }
         }
@@ -223,17 +261,18 @@ public class Grid extends JPanel implements ComponentListener, MouseListener, Mo
         try {
             for (Point newPoint : point) {
                 // Draw new point
-                g.setColor(Color.green);
-                g.fillRect(BLOCK_SIZE + (BLOCK_SIZE*newPoint.x), BLOCK_SIZE + (BLOCK_SIZE*newPoint.y), BLOCK_SIZE, BLOCK_SIZE);
+                g.setColor(Color.black);
+                g.fillRect(BLOCK_SIZE + (BLOCK_SIZE * newPoint.x), BLOCK_SIZE + (BLOCK_SIZE * newPoint.y), BLOCK_SIZE, BLOCK_SIZE);
             }
-        } catch (ConcurrentModificationException cme) {}
+        } catch (ConcurrentModificationException cme) {
+        }
         // Setup grid
         g.setColor(Color.BLACK);
-        for (int i=0; i<=d_gameBoardSize.width; i++) {
-            g.drawLine(((i*BLOCK_SIZE)+BLOCK_SIZE), BLOCK_SIZE, (i*BLOCK_SIZE)+BLOCK_SIZE, BLOCK_SIZE + (BLOCK_SIZE*d_gameBoardSize.height));
+        for (int i = 0; i <= d_gameBoardSize.width; i++) {
+            g.drawLine(((i * BLOCK_SIZE) + BLOCK_SIZE), BLOCK_SIZE, (i * BLOCK_SIZE) + BLOCK_SIZE, BLOCK_SIZE + (BLOCK_SIZE * d_gameBoardSize.height));
         }
-        for (int i=0; i<=d_gameBoardSize.height; i++) {
-            g.drawLine(BLOCK_SIZE, ((i*BLOCK_SIZE)+BLOCK_SIZE), BLOCK_SIZE*(d_gameBoardSize.width+1), ((i*BLOCK_SIZE)+BLOCK_SIZE));
+        for (int i = 0; i <= d_gameBoardSize.height; i++) {
+            g.drawLine(BLOCK_SIZE, ((i * BLOCK_SIZE) + BLOCK_SIZE), BLOCK_SIZE * (d_gameBoardSize.width + 1), ((i * BLOCK_SIZE) + BLOCK_SIZE));
         }
     }
 
